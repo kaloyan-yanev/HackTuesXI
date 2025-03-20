88167ef88e74597eed31d7e1e8445221bf85ef77
gogo112008 <georgirumenoviliev@gmail.com>
2025-03-20 17:12:09 +0200

    Added input to Index.html

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import TimeField,FloatField
+from wtforms import TimeField,FloatField, SubmitField
 from wtforms.validators import DataRequired, Length
 import mysql.connector  
 from string_manipulation import remove_obstacles
@@ -10,7 +10,7 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = 'WhatTheFUCKIsMySQL420',
+    password = 'gogo112008',
     database = 'login_info'
 )
 mycursor = db.cursor(buffered = True)
@@ -18,11 +18,21 @@ mycursor = db.cursor(buffered = True)
 app.config['SECRET_KEY'] = 'secret_key'
 
 class Clock(FlaskForm):
-    time = TimeField('Interval', validators=[DataRequired()])
-@app.route('/')
+    Hours = FloatField('Hours', validators=[DataRequired(), Length(min = 0,max = 24)])
+    minutes = FloatField('minuts', validators=[DataRequired(), Length(min = 0,max = 60)])
+    Submit = SubmitField('Submit')
+@app.route('/', methods=['GET','POST'])
 def Index():
     Timer_info = Clock()
-    return render_template('/Index.html')
+    if Timer_info.validate_on_submit():
+        Session['hours'] = Timer_info.Hours.data
+        Session['minutes'] = Timer_info.minutes.data
+        print(request.form['hours'])
+        print(request.form['minutes'])
+        print(Timer_info.Hours.data)
+        print(Timer_info.minutes.data)
+        return render_template('/Index.html', form = Timer_info)
+    return render_template('/Index.html', form = Timer_info)
 @app.route('/Table')
 def Table():
     return render_template('/Table.html')
