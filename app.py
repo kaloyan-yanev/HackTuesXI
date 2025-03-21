e2ceaaba9b3cab97915cd20370f375594d720340
AdiLand_King <116757159+AdiLandKing@users.noreply.github.com>
2025-03-21 17:46:29 +0200

    gogo mi kaza

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = 'gogo112008',
+    password = 'WhatTheFUCKIsMySQL420',
     database = 'login_info'
 )
 mycursor = db.cursor(buffered = True)
